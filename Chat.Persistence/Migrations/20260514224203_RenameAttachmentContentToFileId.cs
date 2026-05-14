cbee26637afc734cc1e7bc20711ce2f02c8e44b2
SiAvMaAnR <siavmaanr@gmail.com>
2026-05-15 01:42:48 +0300

    feat: migration

---
+++ Chat.Persistence/Migrations/20260514224203_RenameAttachmentContentToFileId.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chat.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameAttachmentContentToFileId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Content",
+                table: "Attachments",
+                newName: "FileId");
+
+            migrationBuilder.Sql("DELETE FROM \"Attachments\";");
+            migrationBuilder.Sql("UPDATE \"Accounts\" SET \"Image\" = NULL;");
+            migrationBuilder.Sql("UPDATE \"Channels\" SET \"Image\" = NULL;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FileId",
+                table: "Attachments",
+                newName: "Content");
+        }
+    }
+}
